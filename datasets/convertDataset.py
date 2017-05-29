48f9818df7436e6df5c29d07c967057049f406a8
João Lucas <Joo Lucas>
2017-05-29 16:33:07 -0300

    Reduced GTD to contain only the needed variables

--- datasets/convertDataset.py
+++
@@ -1,8 +0,0 @@
-from zipfile import ZipFile
-'''
-def extract_zip(input_zip):
-    input_zip=ZipFile(input_zip)
-    return {name: input_zip.read(name) for name in input_zip.namelist()}
-'''
-# References:
-# http://stackoverflow.com/questions/10908877/extracting-a-zipfile-to-memory
