230754ed27832c50e069528a9f6f02c1b3f297a8
João Lucas <Joo Lucas>
2017-05-30 10:40:41 -0300

    Rescaled the map

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var body = d3.select("body");
 
-var spaceObj = {pos: {x: 50, y: 0}, size: {width: 1000, height: 80}, margin: {top: 10, bottom: 40, left: 20, right: 20} };
+var spaceObj = {pos: {x: 0, y: 0}, size: {width: 1000, height: 80}, margin: {top: 10, bottom: 40, left: 20, right: 20} };
 var rangeObj = {min: 1979, max: 2015};
 
 var timeline = new Timeline(spaceObj, rangeObj);
