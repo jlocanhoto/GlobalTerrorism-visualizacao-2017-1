57dc82b6ec2a4762283bd2daf3c2a29d7e4f8c18
João Lucas <Joo Lucas>
2017-05-30 10:46:51 -0300

    Removed leaflet bottom subtitle

--- src/map.js
+++ src/map.js
@@ -17,9 +17,9 @@ class Map{
         this.map = L.map(this.id).setView(latlong, zoomin);
 
         L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoibmlsc29ubGltYSIsImEiOiJjajNhY2wwbXowMHBrMnFvN2NmYWRzeDI4In0.bW4qF_znV3ShcHTG_gQokQ', {
-            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
+            /*attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, \
                           <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, \
-                          Imagery © <a href="http://mapbox.com">Mapbox</a>',
+                          Imagery © <a href="http://mapbox.com">Mapbox</a>',*/
             maxZoom: 18
         }).addTo(this.map);
 
