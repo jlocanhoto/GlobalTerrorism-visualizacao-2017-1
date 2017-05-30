7e1bad11d5351a502413525a315e3a4c6bcc5ddd
João Lucas <jloc@cin.ufpe.br>
2017-05-30 09:09:50 -0300

    Integrated world map with timeline

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 var body = d3.select("body");
 
-var spaceObj = {pos: {x: 50, y: 600}, size: {width: 1000, height: 80}, margin: {top: 10, bottom: 40, left: 20, right: 20} };
+var spaceObj = {pos: {x: 50, y: 100}, size: {width: 1000, height: 80}, margin: {top: 10, bottom: 40, left: 20, right: 20} };
 var rangeObj = {min: 1970, max: 2015};
 
 var timeline = new Timeline(spaceObj, rangeObj);
