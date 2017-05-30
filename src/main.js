59d28b193e084084987244616d580da83d8d7292
João Lucas <Joo Lucas>
2017-05-30 10:30:05 -0300

    Limited timeline from 1979 to 2015

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 var body = d3.select("body");
 
-var spaceObj = {pos: {x: 50, y: 100}, size: {width: 1000, height: 80}, margin: {top: 10, bottom: 40, left: 20, right: 20} };
-var rangeObj = {min: 1970, max: 2015};
+var spaceObj = {pos: {x: 50, y: 0}, size: {width: 1000, height: 80}, margin: {top: 10, bottom: 40, left: 20, right: 20} };
+var rangeObj = {min: 1979, max: 2015};
 
 var timeline = new Timeline(spaceObj, rangeObj);
 timeline.add(body);
