536acba75850492b867c0afb81677952f31d2d87
João Lucas <jloc@cin.ufpe.br>
2017-05-29 23:40:26 -0300

    Modified some margin values

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 var body = d3.select("body");
 
-var spaceObj = {pos: {x: 50, y: 600}, size: {width: 1200, height: 100}, padding: {top: 10, bottom: 10, left: 10, right: 10} };
-var rangeObj = {begin: 1970, end: 2015};
+var spaceObj = {pos: {x: 50, y: 600}, size: {width: 1000, height: 80}, margin: {top: 10, bottom: 40, left: 20, right: 20} };
+var rangeObj = {min: 1970, max: 2015};
 
 var timeline = new Timeline(spaceObj, rangeObj);
 timeline.add(body);
