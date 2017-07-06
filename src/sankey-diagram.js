3a42c38d28f3eb2c3317d41277c62d2d466ed28b
João Lucas <jloc@cin.ufpe.br>
2017-07-06 11:30:55 -0300

    Show only Taliban and Boko Haram

--- src/sankey-diagram.js
+++ src/sankey-diagram.js
@@ -73,7 +73,7 @@ class SankeyDiagram {
 			for (let i = 0; i < keys.length; i++)
 			{
 				let key = keys[i];
-				if (key === "Taliban")
+				if (key === "Taliban" || key === "Boko Haram")
 					this.links = this.links.concat(this.terrorists[key]);
 			}			
 		}
