2f86cad937159c6dbe9bd4849684477c2217adf2
João Lucas <jloc@cin.ufpe.br>
2017-07-06 11:24:07 -0300

    Show only Taliban on Sankey Diagram

--- src/sankey-diagram.js
+++ src/sankey-diagram.js
@@ -73,8 +73,8 @@ class SankeyDiagram {
 			for (let i = 0; i < keys.length; i++)
 			{
 				let key = keys[i];
-				//if (key === "Taliban")
-				this.links = this.links.concat(this.terrorists[key]);
+				if (key === "Taliban")
+					this.links = this.links.concat(this.terrorists[key]);
 			}			
 		}
 
